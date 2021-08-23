202e6cc26cd985bcf4ec7fa401a6f11592017cc0
JiaJun <jiajun.dai@sjsu.edu>
2021-08-22 23:30:14 -0700

    second commit

---
+++ app/src/main/java/com/example/makeitrain/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.makeitrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.icu.text.NumberFormat;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Button makeItRain;
+    private Button showInfo;
+    private TextView moneyValue;
+    private int moneyCounter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        makeItRain = findViewById(R.id.buttonMakeItRain);
+        showInfo = findViewById(R.id.buttonShowInfo);
+        moneyValue = findViewById(R.id.moneyValue);
+
+//        makeItRain.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d("mainActivity", "onClick: Make it rain");
+//            }
+//        });
+
+        //we also can use lambda expression
+        //makeItRain.setOnClickListener(view -> Log.d("mainActivity", "onClick: Make it rain"));
+    }
+
+    public void showMoney(View view) {
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+
+        moneyCounter += 1000;
+        moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
+        Log.d("MIR", "onClick: Make it rain" + moneyCounter);
+    }
+
+    public void showInfo(View view) {
+        //Toast.makeText(MainActivity.this, R.string.app_info, Toast.LENGTH_SHORT).show();
+        Snackbar.make(moneyValue, R.string.app_info, Snackbar.LENGTH_LONG)
+                .setAction("More", view1 -> {
+                    Log.d("Snack", "showInfo: Snackbar More");
+                })
+                .show();
+    }
+}
